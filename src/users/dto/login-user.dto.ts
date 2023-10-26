9283aa82d293b035a1f2c8ebc081382eea0a97c6
tuanhieptran23 <trantuanhiep.work@gmail.com>
2023-10-27 01:12:58 +0700

    sua login-user

--- src/users/dto/login-user.dto.ts
+++ src/users/dto/login-user.dto.ts
@@ -1,10 +1,18 @@
+import { ApiProperty } from "@nestjs/swagger";
 import {  IsNotEmpty,IsEmail } from "class-validator";
 
  export class LoginUserDTO{
    @IsNotEmpty()
    @IsEmail()
+   @ApiProperty({
+    example: 'tuanhieptran@gmail.com',
+    description: 'User email',
+   })
     useremail: string;
 
-    @IsNotEmpty()
+    @ApiProperty({
+      example: '123',
+      description: 'User password',
+    })
     password: string;
  }
