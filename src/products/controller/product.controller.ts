44d26b64349a4904a64dadbe2b33704c21bb2791
tuanhieptran23 <trantuanhiep.work@gmail.com>
2023-10-27 16:38:28 +0700

    thêm description cho controller

--- src/products/controller/product.controller.ts
+++ src/products/controller/product.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Put, Delete, Param, Body, UseGuards } from '@nestjs/common';
 import { ProductService } from '../service/product.service';
 import { ProductDTO } from '../dto/product.dto';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags, ApiOperation } from '@nestjs/swagger';
 import { AuthGuard } from 'src/users/guard/user.guard';
 
 @ApiTags('Product')
@@ -9,28 +9,34 @@ import { AuthGuard } from 'src/users/guard/user.guard';
 @ApiBearerAuth()
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
+
   @UseGuards(AuthGuard)
   @Get()
+  @ApiOperation({ summary: 'Get all products' })
   async findAll() {
     return this.productService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get a product by ID' }) 
   async findById(@Param('id') id: number) {
     return this.productService.findById(id);
   }
 
   @Post()
+  @ApiOperation({ summary: 'Create a new product' }) 
   async create(@Body() ProductDTO: ProductDTO) {
     return this.productService.create(ProductDTO);
   }
 
   @Put(':id')
+  @ApiOperation({ summary: 'Update a product by ID' }) 
   async update(@Param('id') id: number, @Body() ProductDTO: ProductDTO) {
     return this.productService.update(id, ProductDTO);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a product by ID' }) 
   async delete(@Param('id') id: number) {
     return this.productService.delete(id);
   }
--- src/users/controller/user.controller.ts
+++ src/users/controller/user.controller.ts
@@ -7,7 +7,7 @@ import {
 } from '@nestjs/common';
 import { UserService } from '../service/user.service';
 import { UserDTO } from '../dto/register-user.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiOperation } from '@nestjs/swagger'; 
 import { LoginUserDTO } from '../dto/login-user.dto';
 
 @ApiTags('User')
@@ -16,17 +16,20 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('register')
+  @ApiOperation({ summary: 'Register a new user' })
   register(@Body() userDto: UserDTO) {
     return this.userService.register(userDto);
   }
 
   @Post('login')
   @UsePipes(ValidationPipe)
+  @ApiOperation({ summary: 'User login' }) 
   login(@Body() LoginUserDTO: LoginUserDTO): Promise<any> {
     return this.userService.login(LoginUserDTO);
   }
 
   @Post('refresh_token')
+  @ApiOperation({ summary: 'Refresh access token' })
   refreshToken(@Body() { refresh_token }): Promise<any> {
     return this.userService.refreshToken(refresh_token);
   }
