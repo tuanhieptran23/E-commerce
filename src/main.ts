a32a7eee9b17bf782cbb347599838619fac3d389
tuanhieptran23 <trantuanhiep.work@gmail.com>
2023-11-15 16:58:30 +0700

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors();
   const options = new DocumentBuilder()
     .setTitle('E-commerce API')
     .setDescription('API for managing product data')
